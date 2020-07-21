f39c3c3b2ba7eb853344c4a7cbdf08ba30e9bb02
nayraelsalamony <nayraelsalamoney@gmail.com>
2020-07-22 01:55:17 +0200

    first commit

---
+++ src/customers/Info.java
@@ -0,0 +1,34 @@
+
+package customers;
+
+public class Info {
+    private String name ;
+     private String phone ;
+     private String address ;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+     }
+
