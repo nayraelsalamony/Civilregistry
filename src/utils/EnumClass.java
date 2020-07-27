46ce146aeaec1abd13d2e4a8010b8b12df4df3ef
nayraelsalamony <nayraelsalamoney@gmail.com>
2020-07-27 23:24:20 +0200

    creat utils and enum class

---
+++ src/utils/EnumClass.java
@@ -0,0 +1,15 @@
+
+package utils;
+
+public class EnumClass {
+    enum Test{
+     tax
+    }
+    public static void main(String[] args) {
+         Test cost= Test.tax;
+         System.out.println(cost);
+    }
+      
+    
+    
+}
